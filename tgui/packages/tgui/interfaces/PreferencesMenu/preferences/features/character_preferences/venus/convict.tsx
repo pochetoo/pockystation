78be490d1ffa292d2167e9d6a1486c74977e05e9
ariaworld <143797359+ariaworld@users.noreply.github.com>
2025-09-13 20:32:30 +0200

    VENUS (IRIS PORT): Add convict quirk

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/venus/convict.tsx
@@ -0,0 +1,6 @@
+import { Feature, FeatureShortTextInput } from '../../base';
+// IRIS PORT
+export const convict_crime_name: Feature<string> = {
+  name: 'Crime Committed',
+  component: FeatureShortTextInput,
+};
